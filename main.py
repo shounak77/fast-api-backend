e51de2bf408863eac46c2bdf1a9c3bbd6edbc794
shounak77 <51368579+shounak77@users.noreply.github.com>
2024-10-08 23:41:13 +0200

    add logging config
    added config in main.py.

--- main.py
+++ main.py
@@ -5,6 +5,20 @@ from database import engine, DbBase
 from routers.v1.tariff_router import router as tariff_router_v1
 from routers.v2.tariff_router import router as tariff_router_v2
 
+import logging
+import graypy
+
+logger = logging.getLogger("backend_logger")
+logger.setLevel(logging.INFO)
+
+
+graylog_handler = graypy.GELFUDPHandler(host='127.0.0.1', port=12201)
+formatter = logging.Formatter(
+    '{"timestamp": "%(asctime)s", "level": "%(levelname)s", "module": "%(module)s", "message": "%(message)s"}'
+)
+graylog_handler.setFormatter(formatter)
+logger.addHandler(graylog_handler)
+
 app = FastAPI(
     title="Tariff API",
     description="API for CRUD operations of tariffs",
@@ -19,6 +33,7 @@ app.include_router(tariff_router_v2, prefix="/v2", tags=["Tariffs v2"])
 
 @app.get("/")
 def root():
+    logger.info("Root endpoint called")
     return {"message": "Welcome to the Tariff API"}
 
 
