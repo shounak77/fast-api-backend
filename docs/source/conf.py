0647d8f6ad43626106830f5b3ee8131a471f87b1
shounak77 <51368579+shounak77@users.noreply.github.com>
2024-10-09 08:11:41 +0200

    Initialize sphinx
    Initialized sphinx in the root folder.

---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))  # Adjust this path to your project root
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'fast-api-backend'
+copyright = '2024, Shounak'
+author = 'Shounak'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx_autodoc_typehints',
+]
+
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,21 @@
+.. fast-api-backend documentation master file, created by
+   sphinx-quickstart on Tue Oct  8 23:19:52 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+fast-api-backend documentation
+==============================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Documentation for helper functions
+
+
+.. automodule:: utils
+   :members:
+   :undoc-members:
+   :show-inheritance:
